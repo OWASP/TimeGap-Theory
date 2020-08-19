08b96b3eb4278f2f6a5ac7729dda99500d3a2eab
Abhi-M <abhimbalakrishnan@gmail.com>
2020-08-19 14:23:17 -0700

    Update footer.php

--- timegaptheory/common/footer.php
+++ timegaptheory/common/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
       <div class="content has-text-centered" style="font-size:0.9em;">
-        <a href="https://www2.owasp.org/www-project-timegap-theory/">OWASP</a> | <a href="//timegaptheory.com">TimeGap Theory</a> | <a href="https://en.wikipedia.org/wiki/Time-of-check_to_time-of-use">TOCTOU</a> | <a href="https://resources.securitycompass.com/blog/moving-beyond-the-owasp-top-10-part-1-race-conditions-2">Race Condition</a><br>
+        <a href="https://www.owasp.org/www-project-timegap-theory/">OWASP</a> | <a href="//timegaptheory.com">TimeGap Theory</a> | <a href="https://en.wikipedia.org/wiki/Time-of-check_to_time-of-use">TOCTOU</a> | <a href="https://resources.securitycompass.com/blog/moving-beyond-the-owasp-top-10-part-1-race-conditions-2">Race Condition</a><br>
         <br>
         <a id="snow" onclick="togglesnow();">Snow off</a> | <a id="darkb" onclick="toggleDark();">Dark mode off</a>
         <?php require_once dirname( __FILE__ ) . '/' . '../score/track.php'; ?>
